8ff530a4db69bf2bd0c28d597cc8e6f32c9d6d51
alphanse <alphanse98@gmail.com>
2022-07-22 14:29:03 +0530

    add

---
+++ files/app.js
@@ -0,0 +1,24 @@
+let a = document.querySelector('.p1');
+let b = document.querySelector('.p12');
+let c = document.querySelector('.p13');
+let d = document.querySelector('.p14');
+
+document.querySelector(".btn").addEventListener("click", function () {
+
+    var arr = ['A', 'B','C', 'D']
+    //a.innerHTML = arr[1];
+    // var e = 
+   for(i=0;i<=arr.length;i++){
+    var rendomNum = Math.floor(Math.random()*arr.length)
+    while()
+
+    a.innerHTML = arr[rendomNum];
+    b.innerHTML = arr[rendomNum];
+    c.innerHTML = arr[rendomNum];
+    d.innerHTML = arr[rendomNum];
+   }
+
+    
+    
+
+});
